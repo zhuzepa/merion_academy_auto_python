a4f428bb73eb9643d795a674a5d652c3b645a562
Prince Aleksey <chirkinaa@pgk.ru>
2024-08-01 22:26:21 +0300

    add req

--- home_work/hw11/test_employee.py
+++ home_work/hw11/test_employee.py
@@ -8,8 +8,7 @@ base_url = "https://x-clients-be.onrender.com"
 def test_emp_create(get_token):
     employee = EmployeeService()
     employee.company_id()
-    employee.creating_company_employee(get_token, '8909999999', "Заебатов", "Бабулат")
+    id_user= employee.creating_company_employee(get_token, '8909999999', "Заебатов", "Бабулат")
     employee.get_list_of_employees()
-
-
-# employee.creating_company_employee(employee.company_id)
+    employee.change_data_employee(get_token, id_user, True, 'pethov@pethov.ii', '8999999999')
+    employee.ban_creating_company_employee(get_token, '23242', 'Бан', 'Банов', 123)
